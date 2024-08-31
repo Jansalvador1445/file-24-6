aa0f329b0858d41379fa19c893fb3a15d7ad47d9
Jan Salvador Sebastian <sebastian@mclinica.com>
2024-08-31 13:04:49 +0800

    Added more variable and print

--- Main.java
+++ Main.java
@@ -2,7 +2,9 @@ class Main {
     public static void main(String [] args){
 
         String subject = "ITE 300";
+        String section = "Block 6";
 
         System.out.println(subject); 
+        System.out.println(section); 
     }
 }
